09eb6642ee680d6897a348acd59a2118bc79e07e
naeglinghaff <kate.o.haffenden@gmail.com>
2020-03-17 14:56:03 +0000

    Added turnUp function

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -9,5 +9,9 @@ describe('Thermostat', function(){
     it('starts at 20', function(){
       expect(thermostat.temperature).toEqual(20);
     })
+    it('can change the temperature', function(){
+      expect(thermostat.turnUp()).toEqual(21);
+    })
   })
+
 })
--- src/thermostat.js
+++ src/thermostat.js
@@ -6,3 +6,8 @@ function Thermostat(){
 Thermostat.prototype.temperature = function() {
   return 20;
 }
+
+Thermostat.prototype.turnUp = function() {
+  this.temperature++;
+  return this.temperature;
+}

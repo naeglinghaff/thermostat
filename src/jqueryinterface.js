4ee42ad86657834e211595578d1f74d4b1ca12e6
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-03-17 17:59:15 +0000

    added jquery functionality for power_save and turn up and down

--- src/jqueryinterface.js
+++ src/jqueryinterface.js
@@ -1,6 +1,35 @@
 $(document).ready(function() {
+  
+  let thermostat = new Thermostat;
+
   $("#myButton").click(function (e) {
     alert("Button is Clicked")
-});
+  });
+
+  function changeValue(num) {
+    $("#temp_display").text(num);
+  }
+
+  $("#turn_up").click(function () {
+    thermostat.turnUp();
+    changeValue(thermostat.temperature);
+  });
+
+  $("#turn_down").click(function() {
+    thermostat.turnDown();
+    changeValue(thermostat.temperature);
+  });
+
+  $("#power_saving_mode_on").click(function() {
+    thermostat.powerSave('on');
+    $("#power_saving_mode_on").css("background-color", "green");
+    $("#power_saving_mode_off").css("background-color", "white")
+  });
+
+  $("#power_saving_mode_off").click(function() {
+    thermostat.powerSave('off');
+    $("#power_saving_mode_off").css("background-color", "red")
+    $("#power_saving_mode_on").css("background-color", "white");
+  })
 })
 
